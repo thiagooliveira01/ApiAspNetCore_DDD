78dfb6c7962e867af424ff7212efd9b852d159e6
Thiago <tuim_gm@hotmail.com>
2020-03-05 22:34:13 -0300

    LoginService

---
+++ src/Api.Service/Services/LoginService.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Api.Domain.Repository;
+
+namespace Api.Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repository;
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> FindByLogin(UserEntity user)
+        {
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);
+            }
+            return null;
+        }
+    }
+}
