3b712572ff2701d1ac39cc5b10c28c79944db1c1
Thiago <tuim_gm@hotmail.com>
2020-03-11 21:42:36 -0300

    TokenConfigurations

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
+using Api.Domain.Security;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace Application
@@ -28,6 +30,15 @@ namespace Application
         {
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);//services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            var signingConfigurations = new SigningConfigurations();
+            services.AddSingleton(signingConfigurations);
+
+            var tokenConfigurations = new TokenConfigurations();
+            new ConfigureFromConfigurationOptions<TokenConfigurations>(
+                Configuration.GetSection("TokenConfigurations"))
+                .Configure(tokenConfigurations);
+            services.AddSingleton(tokenConfigurations);
 
             services.AddSwaggerGen(c =>
             {
--- src/Api.Service/Services/LoginService.cs
+++ src/Api.Service/Services/LoginService.cs
@@ -1,25 +1,106 @@
+using System;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
+using Api.Domain.Dtos;
 using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Api.Domain.Repository;
+using Api.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Service.Services
 {
     public class LoginService : ILoginService
     {
         private IUserRepository _repository;
-        public LoginService(IUserRepository repository)
+        private SigningConfigurations _signingConfigurations;
+        private TokenConfigurations _tokenConfigurations;
+        private IConfiguration _configuration { get; }
+
+        public LoginService(IUserRepository repository,
+                            SigningConfigurations signingConfigurations,
+                            TokenConfigurations tokenConfigurations,
+                            IConfiguration configuration)
         {
             _repository = repository;
+            _signingConfigurations = signingConfigurations;
+            _tokenConfigurations = tokenConfigurations;
+            _configuration = configuration;
         }
 
-        public async Task<object> FindByLogin(UserEntity user)
+        public async Task<object> FindByLogin(LoginDto user)
         {
             if (user != null && !string.IsNullOrWhiteSpace(user.Email))
             {
-                return await _repository.FindByLogin(user.Email);
+                var baseUser = new UserEntity();
+                baseUser = await _repository.FindByLogin(user.Email);
+                if (baseUser == null)
+                {
+                    return new
+                    {
+                        authenticated = false,
+                        message = "Falha ao autenticar"
+                    };
+                }
+                else
+                {
+                    var identity = new ClaimsIdentity(
+                        new GenericIdentity(user.Email),
+                        new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //jti id do Token
+                            new Claim(JwtRegisteredClaimNames.UniqueName, user.Email)
+                        }
+                    );
+
+                    DateTime createDate = DateTime.Now;
+                    DateTime expirationDate = createDate + TimeSpan.FromSeconds(_tokenConfigurations.Seconds);
+
+                    var handler = new JwtSecurityTokenHandler();
+
+                    var token = CreateToken(identity, createDate, expirationDate, handler);
+
+                    return SucessObject(createDate, expirationDate, token, user);
+                }
+
             }
             return null;
         }
+
+        private object SucessObject(DateTime createDate, DateTime expirationDate, string token, LoginDto user)
+        {
+            return new
+            {
+                authenticated = true,
+                created = createDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                expiration = expirationDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                acessToken = token,
+                userName = user.Email,
+                message = "Usuário Logado com sucesso"
+            };
+        }
+
+        private string CreateToken(ClaimsIdentity identity, DateTime createDate, DateTime expirationDate, JwtSecurityTokenHandler handler)
+        {
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _tokenConfigurations.Issuer,
+                Audience = _tokenConfigurations.Audience,
+                SigningCredentials = _signingConfigurations.SigningCredentials,
+                Subject = identity,
+                NotBefore = createDate,
+                Expires = expirationDate
+            });
+
+            var token = handler.WriteToken(securityToken);
+
+            return token;
+
+        }
+
+
     }
 }
